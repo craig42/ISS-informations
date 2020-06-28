52d8bae4b387c2ccb9a851c72b3e7148cc118b7d
Craig Josse <craig.josse@icloud.com>
2020-06-28 22:42:48 +0200

    add cool timer to see ISS progression

--- ISS informations/View/MainView.swift
+++ ISS informations/View/MainView.swift
@@ -10,14 +10,22 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
-
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var peopleInSpaceLabel: UILabel!
     @IBOutlet var issUpcoming: UILabel!
+    var timer = Timer()
     // https://github.com/public-apis/public-apis
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        timer = Timer.scheduledTimer(withTimeInterval: 15, repeats: true) {_ in
+            self.updateISSPosition()
+            self.updateNumberOfPeopleInSpace()
+            self.updateIssPassTime()
+        }
+        timer.fire()
+    }
+    func updateISSPosition() {
         let issPosition = IssPositionToView()
         issPosition.getIssPosition(callback: { coordinate in
             let annotation = MKPointAnnotation()
@@ -31,8 +39,9 @@ class ViewController: UIViewController, MKMapViewDelegate {
                                                 span: MKCoordinateSpan(latitudeDelta: 100.0, longitudeDelta: 100.0))
                 self.mapView.setRegion(region, animated: true)
             }
-
         })
+    }
+    func updateNumberOfPeopleInSpace() {
         let peopleInSpace = PeopleInSpaceToView()
         peopleInSpace.getPeopleInSpace(callback: { peopleText in
             DispatchQueue.main.async {
@@ -41,14 +50,15 @@ class ViewController: UIViewController, MKMapViewDelegate {
                 self.peopleInSpaceLabel.numberOfLines = numberOfLines.count
             }
         })
+    }
+    func updateIssPassTime() {
         let issPassTime = IssPassTimesToView()
         issPassTime.getIssPassTimes(callback: { passTimeText in
-                DispatchQueue.main.async {
-                    self.issUpcoming.text = passTimeText
-                    let numberOfLines = passTimeText.components(separatedBy: "\n")
-                    self.issUpcoming.numberOfLines = numberOfLines.count
+            DispatchQueue.main.async {
+                self.issUpcoming.text = passTimeText
+                let numberOfLines = passTimeText.components(separatedBy: "\n")
+                self.issUpcoming.numberOfLines = numberOfLines.count
             }
         })
     }
-
 }
